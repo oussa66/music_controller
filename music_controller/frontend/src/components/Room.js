ca379b1f889439a1346e6fcf19c51f14a114144d
oussama <oussama.talebbenkhlouf@gmail.com>
2020-11-26 14:59:11 +0100

    fuck

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,21 +1,23 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 export default class Room extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            votesToSkip:2,
-            guestCanPause:false,
-            isHost:false,
-        }
-        this.roomCode=this.props.match.params.roomCode;
+  constructor(props) {
+    super(props);
+    this.state = {
+      votesToSkip: 2,
+      guestCanPause: false,
+      isHost: false,
+    };
+    this.roomCode = this.props.match.params.roomCode;
     }
-    render(){
-        return <div>
-            <p>{this.roomCode}</p>
+    render() {
+        return (
+          <div>
+            <h3>{this.roomCode}</h3>
             <p>Votes: {this.state.votesToSkip}</p>
-            <p>Guest can pause: {this.state.guestCanPause}</p>
-            <p>Host: {this.state.isHost}</p>
-        </div>
-    }
+            <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+            <p>Host: {this.state.isHost.toString()}</p>
+          </div>
+        );
+      }
 }
